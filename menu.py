efe4db873428e2f1b97a6dfe30b765948bae8d6c
god-archer <1040257528@qq.com>
2025-03-10 12:32:50 +0800

    update:完善README，requirements。 增加版权声明与许可证声明 ->next:未定

--- menu.py
+++ menu.py
@@ -1,3 +1,20 @@
+# TreeGo - A board game
+# This file is part of TreeGo
+# Copyright (C) 2024 God_archer (1040257528@qq.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pygame
 from config import SCREEN_WIDTH, SCREEN_HEIGHT, WHITE, BLACK, BUTTON_BG, BUTTON_HOVER, BUTTON_SHADOW, BUTTON_TEXT
 
